1d5390d4a771d2abd896ffa98971d8c5a1d150cf
murad44 <murad_amanov189@icloud.com>
2020-11-27 21:33:59 +0400

    slider

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$('#MySlider1').slick({
+    dots: true,
+    infinite: true,
+    centerMargin: '60px',
+    speed: 300,
+    slidesToShow: 3,
+    centerMode: true,
+    variableWidth: true
+  });
+          
+  
